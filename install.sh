2a56c3025cee06e461e141a6c95ccbb3612c807c
yuma.io <endoyuma.world@gmail.com>
2020-02-03 02:36:38 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 sudo apt update
-sudo apt -y install git curl g++ make
-sudo apt -y install zlib1g-dev libssl-dev libreadline-dev libyaml-dev libxml2-dev libxslt-dev sqlite3 libsqlite3-dev 
-sudo apt -y install nodejs autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm5 libgdbm-dev
+sudo apt -y install git curl g++ make build-essential apt-utils nodejs
+sudo apt -y install zlib1g-dev libssl-dev libreadline-dev libyaml-dev libxml2-dev libxslt-dev sqlite3 sqlite3-doc libsqlite3-dev 
+sudo apt -y install autoconf bison libssl-dev libyaml-dev libreadline-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm5 libgdbm-dev
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 mkdir ~/.rbenv/plugins
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
