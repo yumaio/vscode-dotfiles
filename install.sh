5314349556f328fdfc4b4e3645e3994b8ed98a8b
yuma.io <endoyuma.world@gmail.com>
2020-02-02 23:31:09 +0900

    add install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+sudo apt-get update
+sudo apt-get -y install git curl g++ make
+sudo apt-get -y install zlib1g-dev libssl-dev libreadline-dev
+sudo apt-get -y install libyaml-dev libxml2-dev libxslt-dev
+sudo apt-get -y install sqlite3 libsqlite3-dev nodejs
+cd
+git clone https://github.com/sstephenson/rbenv.git .rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+exec $SHELL
+mkdir -p ~/.rbenv/plugins
+cd ~/.rbenv/plugins
+git clone https://github.com/sstephenson/ruby-build.git
+rbenv install 2.6.3
+rbenv global 2.6.3
